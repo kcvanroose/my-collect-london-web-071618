66264d238bc5dcfbf56c48b1036fa2c775de93d7
casey van-roose <kcvanroose@gmail.com>
2018-07-08 17:44:42 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,10 @@ def my_collect(array)
     new_array = []
     i = 0
     while i < array.length
-    yield(array[i])
-    new_array.push(array[i])
+    item = yield(array[i])
+    new_array.push(item)
     i = i + 1
     end
   new_array
-end
+  end
 end
